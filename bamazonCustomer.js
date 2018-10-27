c7fe44b6a50040d7da797021ef9804ebcc343484
Mustafa Tunc <mtunc@hotmail.com>
2018-10-27 09:01:32 -0500

    bamazon is almost ready

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var Table = require('cli-table');
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
@@ -12,182 +13,270 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "",
-  database: "top_songsDB"
+  database: "bamazon"
 });
+
+connection.connect(function (err) {
+  if (err) throw err;
+  connection.query("SELECT*FROM products", function (err, res) {
+    // console.log("item_id" + "  "+"product_name"+"  "  + "department_name" +"  "+ "price" +"  " +"stock_quantity" )
+
+    // for (i=0; i<res.length; i++) {
+
+    //     console.log(res[i].item_id +"        "+ res[i].product_name + "      "+res[i].department_name +"      " +res[i].price + "          " + res[i].stock_quantity);
+    // }
+    // console.log(res);
+    //  connection.end();
+    // bamazon();
+
+
+    var table = new Table({
+      head: ['item_id', 'product_name', 'department_name', 'price', 'stock_quantity'],
+      colWidths: [10, 20, 20, 20, 20]
+    });
+
+    // table is an Array, so you can `push`, `unshift`, `splice` and friends
+    for (i = 0; i < res.length; i++) {
+
+      table.push(
+        [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+      );
+
+    }
+
+    console.log(table.toString());
+    //connection.end();
+  });
+  //bamazon();
+});
+
+ 
+
 
 // connection.connect(function(err) {
 //   if (err) throw err;
-//   runSearch();
+//   // run the start function after the connection is made to prompt the user
+  start();
 // });
 
-// function runSearch() {
-//   inquirer
-//     .prompt({
-//       name: "action",
-//       type: "rawlist",
-//       message: "What would you like to do?",
-//       choices: [
-//         "Find songs by artist",
-//         "Find all artists who appear more than once",
-//         "Find data within a specific range",
-//         "Search for a specific song",
-//         "Find artists with a top song and top album in the same year"
-//       ]
-//     })
-//     .then(function(answer) {
-//       switch (answer.action) {
-//       case "Find songs by artist":
-//         artistSearch();
-//         break;
+// function which prompts the user for what action they should take
+function start() {
+  inquirer
+    .prompt({
+      name: "buyOrSell",
+      type: "rawlist",
+      message: "Would you like to [BUY] an auction or [Sell] on an auction?",
+      choices: ["BUY", "SELL"]
+    })
+    .then(function(answer) {
+      // based on their answer, either call the bid or the post functions
+      if (answer.buyOrSell.toUpperCase() === "BUY") {
+        buyItem();
+      }
+      else {
+        sellItem();
+      }
+    });
+}
 
-//       case "Find all artists who appear more than once":
-//         multiSearch();
-//         break;
 
-//       case "Find data within a specific range":
-//         rangeSearch();
-//         break;
 
-//       case "Search for a specific song":
-//         songSearch();
-//         break;
+// function buyItem() {
+//   // query the database for all items being auctioned
+//   connection.query("SELECT * FROM products", function(err,res) {
+    //if (err) throw err;
+    // once you have the items, prompt the user for which they'd like to bid on
+    // inquirer
+    // .prompt({
+    //   name: "choice",
+    //   type: "rawlist",
+    //   message: "Which item would you like to buy?",
+    //   choices: ["1", "2","3","4","5","6","7","8","9","10"]
+    // })
+    // .then(function(answer) {
+    //   // based on their answer, either call the bid or the post functions
+    //   if (choices === "1") {
+    //     item1();
+    //   }
+    //   else if (choices === "2") {
+    //     item2();  
+    //   }
+    //   else if (answer.whichItem() === "3") {
+    //     item3();  
+    //   }
+    //   else if (answer.whichItem() === "4") {
+    //     item4();  
+    //   }
+    //   else if (answer.whichItem() === "5") {
+    //     item5();  
+    //   }
+    //   else if (answer.whichItem() === "6") {
+    //     item6();  
+    //   }
+    //   else if (answer.whichItem() === "7") {
+    //     item7();  
+    //   }
+    //   else if (answer.whichItem() === "8") {
+    //     item8();  
+    //   }
+    //   else if (answer.whichItem() === "9") {
+    //     item9();  
+    //   }
+    //   else if (answer.whichItem() === "10") {
+    //     item10();  
+    //   }
 
-//       case "Find artists with a top song and top album in the same year":
-//         songAndAlbumSearch();
-//         break;
-//       }
-//     });
-// }
+    // });
+  
 
-// function artistSearch() {
-//   inquirer
-//     .prompt({
-//       name: "artist",
-//       type: "input",
-//       message: "What artist would you like to search for?"
-//     })
-//     .then(function(answer) {
-//       var query = "SELECT position, song, year FROM top5000 WHERE ?";
-//       connection.query(query, { artist: answer.artist }, function(err, res) {
-//         for (var i = 0; i < res.length; i++) {
-//           console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
-//         }
-//         runSearch();
-//       });
-//     });
-// }
 
-// function multiSearch() {
-//   var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
-//   connection.query(query, function(err, res) {
-//     for (var i = 0; i < res.length; i++) {
-//       console.log(res[i].artist);
-//     }
-//     runSearch();
-//   });
-// }
+// function to handle posting new items up for auction
+function sellItem() {
+  // prompt for info about the item being put up for auction
+  inquirer
+    .prompt([
+      {
+        name: "item",
+        type: "input",
+        message: "What is the item_id you would like to submit?"
+      },
+      {
+        name: "product",
+        type: "input",
+        message: "What is the product name would you like to sell in bamazon?"
+      },
+      {
+        name: "department",
+        type: "input",
+        message: "What is the department name would you like to place your item?"
+      },
+      {
+        name: "price",
+        type: "input",
+        message: "What is the price of your item?",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+         return false;
+        }
+      },
+      
+        {
+          name: "stock",
+          type: "input",
+          message: "how many items do you have in your stock?",
+          validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+           return false;
+          }
+        },
+      
+    ])
+    .then(function(answer) {
+      // when finished prompting, insert a new item into the db with that info
+      connection.query(
+        "INSERT INTO products SET ?",
+        {
+          item_id: answer.item,
+          product_name: answer.product,
+          department_name:answer.department,
+          price: answer.price,
+          stock_quantity: answer.stock
+        },
+        function(err) {
+          if (err) throw err;
+          console.log("Your item was created successfully!");
+          // re-prompt the user for if they want to bid or post
+          start();
+        }
+      );
+      connection.end();
+    });
+}
 
-// function rangeSearch() {
-//   inquirer
-//     .prompt([
-//       {
-//         name: "start",
-//         type: "input",
-//         message: "Enter starting position: ",
-//         validate: function(value) {
-//           if (isNaN(value) === false) {
-//             return true;
-//           }
-//           return false;
-//         }
-//       },
-//       {
-//         name: "end",
-//         type: "input",
-//         message: "Enter ending position: ",
-//         validate: function(value) {
-//           if (isNaN(value) === false) {
-//             return true;
-//           }
-//           return false;
-//         }
-//       }
-//     ])
-//     .then(function(answer) {
-//       var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-//       connection.query(query, [answer.start, answer.end], function(err, res) {
-//         for (var i = 0; i < res.length; i++) {
-//           console.log(
-//             "Position: " +
-//               res[i].position +
-//               " || Song: " +
-//               res[i].song +
-//               " || Artist: " +
-//               res[i].artist +
-//               " || Year: " +
-//               res[i].year
-//           );
-//         }
-//         runSearch();
-//       });
-//     });
-// }
 
-// function songSearch() {
-//   inquirer
-//     .prompt({
-//       name: "song",
-//       type: "input",
-//       message: "What song would you like to look for?"
-//     })
-//     .then(function(answer) {
-//       console.log(answer.song);
-//       connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function(err, res) {
-//         console.log(
-//           "Position: " +
-//             res[0].position +
-//             " || Song: " +
-//             res[0].song +
-//             " || Artist: " +
-//             res[0].artist +
-//             " || Year: " +
-//             res[0].year
-//         );
-//         runSearch();
-//       });
-//     });
-// }
 
-// function songAndAlbumSearch() {
-//   inquirer
-//     .prompt({
-//       name: "artist",
-//       type: "input",
-//       message: "What artist would you like to search for?"
-//     })
-//     .then(function(answer) {
-//       var query = "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
-//       query += "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
-//       query += "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year ";
+//////////////////////////////////
+function buyItem() {
+  // prompt for info about the item being put up for auction
+  inquirer
+    .prompt([
+      {
+        name: "item",
+        type: "input",
+        message: "What is the item_id you would like to buy?",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+         return false;
+        }
+      },
+      {
+        name: "price",
+        type: "input",
+        message: "What is the price of your item?",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+         return false;
+        },
+      },
 
-//       connection.query(query, [answer.artist, answer.artist], function(err, res) {
-//         console.log(res.length + " matches found!");
-//         for (var i = 0; i < res.length; i++) {
-//           console.log(
-//             "Album Position: " +
-//               res[i].position +
-//               " || Artist: " +
-//               res[i].artist +
-//               " || Song: " +
-//               res[i].song +
-//               " || Album: " +
-//               res[i].album +
-//               " || Year: " +
-//               res[i].year
-//           );
-//         }
+      {
+        name: "stock",
+        type: "input",
+        message: "how many items do you have in your stock?",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+         return false;
+        }
+      },
+    
+      {
+        name: "amount",
+        type: "input",
+        message: "How many would you like  to buy?",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+         return false;
+        }
+      },
+      
+      
+    ])
+    .then(function(answer) {
+      // when finished prompting, insert a new item into the db with that info
+      connection.query(
+        "SELECT * FROM products ",
+        {
+          item_id: answer.item,
+          price: answer.price,
+          //stock_quantity: answer.stock,
+         
+          amount: answer.amount,
+          
+        },
+        
+        function(err) {
+          if (err) throw err;
+           let total = 0;
+           total += answer.price * answer.amount
+          console.log("There is a glitch in our system please try again later!");
+          // re-prompt the user for if they want to bid or post
+          start();
+        })      
+        connection.end();
+    });
+}
 
-//         runSearch();
-//       });
-//     });
-// }
+    
+    
